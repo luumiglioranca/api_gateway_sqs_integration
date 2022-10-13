4b42a0b913f7d28582883adf2a4ba85966bd2ba7
Lucas Migliorança <41163225+luumiglioranca@users.noreply.github.com>
2022-10-13 17:09:14 -0300

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -1,6 +1,6 @@
 
 # Get Certificate SSL (ACM)
-data "aws_acm_certificate" "edtech_ssl" {
+data "aws_acm_certificate" "certificate_ssl" {
 
   domain   = "*.${var.domain_name}"
   statuses = ["ISSUED"]
